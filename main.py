191fbf62f3b52973796444272c2153ad93d6e4a2
PedramHashemi <s.pedram.h@gmail.com>
2021-09-22 15:19:06 +0200

    correcting scraper

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from scraper import scraper
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return scraper()
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -3,53 +3,54 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-numbers = {
-    'keine': '0',
-    'eins': '1',
-    'einen': '1',
-    'zwei': '2',
-    'drei': '3',
-    'vier': '4',
-    'fünf': '5',
-    'sechs': '6',
-    'sieben': '7',
-    'acht': '8',
-    'neun': '9',
-    'zehn': '10',
-    'elf': '11',
-    'zwölf': '12'
-}
 
-url = 'https://www.medizin.uni-tuebingen.de/de/hinweise-corona-virus/corona-live-ticker'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-times = soup.find_all("ul", class_="list-bullets timeline")
-times = times[0].find_all('li')
-dates = []
-infected = []
-intensive = []
-for i in range(len(times)):
-    # for i in range(40):
-    print(i)
-    li = times[i]
-    date = li.find('span').text
-    comment = li.text.split()
-    dates.append(date)
-    if comment[1] == 'versorgt':
-        infected_count = comment[5]
-        try:
-            intensive_count = comment[comment.index('davon') + 1]
-        except:
-            intensive_count = infected_count
+def scraper():
+    numbers = {
+        'keine': '0',
+        'eins': '1',
+        'einen': '1',
+        'zwei': '2',
+        'drei': '3',
+        'vier': '4',
+        'fünf': '5',
+        'sechs': '6',
+        'sieben': '7',
+        'acht': '8',
+        'neun': '9',
+        'zehn': '10',
+        'elf': '11',
+        'zwölf': '12'
+    }
 
-        print(infected_count)
-        print(intensive_count)
-        try:
-            infected.append(int(infected_count) if infected_count.isdigit() else int(numbers[infected_count]))
-            intensive.append(int(intensive_count) if intensive_count.isdigit() else int(numbers[intensive_count]))
-        except:
-            infected.append(int(infected_count) if infected_count.isdigit() else int(numbers[infected_count]))
-            intensive.append(int(intensive_count) if intensive_count.isdigit() else int(numbers[intensive_count]))
-        finally:
-            infected.append(0)
-            intensive.append(0)
+    url = 'https://www.medizin.uni-tuebingen.de/de/hinweise-corona-virus/corona-live-ticker'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    times = soup.find_all("ul", class_="list-bullets timeline")
+    times = times[0].find_all('li')
+    dates = []
+    infected = []
+    intensive = []
+    for i in range(len(times)):
+        # for i in range(40):
+        print(i)
+        li = times[i]
+        date = li.find('span').text
+        comment = li.text.split()
+        dates.append(date)
+        if comment[1] == 'versorgt':
+            infected_count = comment[5]
+            try:
+                intensive_count = int(comment[comment.index('davon') + 1])
+            except:
+                intensive_count = infected_count
+
+            print(infected_count)
+            print(intensive_count)
+            try:
+                infected.append(int(infected_count) if infected_count.isdigit() else int(numbers[infected_count]))
+                intensive.append(int(intensive_count) if intensive_count.isdigit() else int(numbers[intensive_count]))
+            except:
+                infected.append(0)
+                intensive.append(0)
+
+    return {'date': dates, 'infected': infected, 'intensive': intensive}
