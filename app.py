eeea1c0ef75ee1a08d775d52a7fa787e6856843b
PedramHashemi <s.pedram.h@gmail.com>
2021-11-28 22:49:00 +0100

    adding the plot

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 from scraper import scraper
+import json
+import plotly
+import plotly.graph_objects as go
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return scraper()
+    covid_data = scraper()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=covid_data['date'][::-1],
+        y=covid_data['infected'][::-1],
+        name='Infected',
+        line=dict(color='firebrick', width=1)))
+    fig.add_trace(go.Scatter(
+        x=covid_data['date'][::-1],
+        y=covid_data['intensive'][::-1],
+        name='Intensive',
+        mode='lines',
+        line=dict(color='royalblue', width=1)))
+
+    fig.show()
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('notdash.html', graphJSON=graphJSON)
 
 
 if __name__ == "__main__":
+    stop = 1
     app.run(debug=True)
